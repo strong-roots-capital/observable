ad26cdd1bdf6b479c4e1d4c235aa23b48ca12959
Eric Crosson <eric.s.crosson@utexas.edu>
2019-02-27 14:58:33 -0600

    Import code from scratch space

--- src/observable.ts
+++ src/observable.ts
@@ -3,10 +3,58 @@
  * Observe and react to EventSources
  */
 
+namespace debug {
+    export const observer = require('debug')('observer')
+}
+
+import { EventSource, DEFAULT_EVENT } from './event-source'
 
 /**
- * TODO: document
+ * DOCUMENT
  */
-export default function observable() {
-    // TODO: implement
+interface Observer {
+    dispose: () => void
+}
+
+/**
+ * DOCUMENT
+ */
+const VoidObservable = { dispose: () => {debug.observer('disposed')} }
+
+// TODO: provide a Mixin version
+/**
+ * DOCUMENT
+ */
+export default class Observable {
+
+    protected observer: Observer = VoidObservable
+
+    /**
+     * DOCUMENT
+     */
+    observe(ee: EventSource, callback: any): Observer {
+        debug.observer('observing')
+        ee.on(DEFAULT_EVENT, callback)
+        return {
+            dispose: () => {
+                ee.removeListener(DEFAULT_EVENT, callback)
+                debug.observer('disposed')
+            }
+        }
+    }
+
+    /**
+     * DOCUMENT
+     */
+    set source(source: EventSource) {
+        this.observer.dispose()
+        this.observer = this.observe(source, this.handleEvent)
+    }
+
+    /**
+     * DOCUMENT
+     */
+    handleEvent(event: string | symbol, ...args: any[]) {
+        debug.observer('Unhandled event', event, args)
+    }
 }
